151ba00907cb23f38fb9cd1586d08a14ce3e4e82
chaimae <maakoulchaimae4@gmail.com>
2026-03-27 09:29:50 +0100

    tp4 docker

---
+++ db/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS flaskdb ;
+USE flaskdb ;
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email text
+);
+INSERT INTO users (name) VALUES ("SALIM"),("NOUR"),("YOUSSEF");
